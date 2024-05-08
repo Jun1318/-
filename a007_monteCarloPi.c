c18762126d34646c823c1827f9a3a9b3de314cfc
Jun1318 <165673368+Jun1318@users.noreply.github.com>
2024-05-08 19:52:19 +0900

    Create a007_monteCarloPi.c

---
+++ a007_monteCarloPi.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#include<math.h>
+#include<stdbool.h>
+int compare(int a){
+	for(int r=1;r<10;r++)	if(a==pow(10, r))	return true;
+	return false;
+}
+int main(){
+	int x, y, total, r = 50;
+	srand((unsigned)time(NULL));
+	for(int a=1;a<=100000000;a++){
+		x = rand()%50+1;
+		y = rand()%50+1;
+		if((x-r)*(x-r)+(y-r)*(y-r)<= r*r){
+			total++;
+		}
+		if(compare(a))	printf("%d에서의 원주율 : %f\n", a, (float)total/a*4);
+	}
+}
