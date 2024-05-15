d2c8a5cc759314b07801d8f5cadc9c3b7f47323d
Jun1318 <165673368+Jun1318@users.noreply.github.com>
2024-05-15 18:58:01 +0900

    Update c021|20240515.c

--- c021|20240515.c
+++ c021|20240515.c
@@ -5,7 +5,7 @@ int main(void){
 	bool memory[5000] = {0,};
 	int person_num, start_num, present_num,count=0, roll;
 	scanf("%d%*c%d", &person_num, &start_num); // 7 3
-	present_num = start_num; // 3 3
+	present_num = start_num-1; // 2 2
 	for(int a=0;count<person_num;a++){ // 0 < 7
 		roll = 0;
 		for(int b=0;b<start_num;){
