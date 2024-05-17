371bef8b3d6cbe275fd3523a27df2ad3533a2f3c
Jun1318 <165673368+Jun1318@users.noreply.github.com>
2024-05-17 13:51:52 +0900

    Create c023|20240517.c

---
+++ c023|20240517.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(void){
+	char input[100];
+	int alpha[26];
+	for(int a=0;a<26;a++)	alpha[a] = -1;		
+	scanf("%s%*c", input);
+	for(int a=0;input[a] != NULL;a++){
+		int temp = (int)input[a];
+		if(alpha[temp-97] == -1)	alpha[temp-97] = a; 
+	}
+	for(int b =0;b<26;b++)	printf("%c : %d%c", char(b+97), alpha[b], (b%10==9)?'\n':'\t');
+}
