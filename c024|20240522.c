065afc2d6043871135d2f0f015a6b8ca492d70b1
Jun1318 <165673368+Jun1318@users.noreply.github.com>
2024-05-22 01:49:24 +0900

    Create c024|20240522.c

---
+++ c024|20240522.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#define size 1000000
+int main(void){
+	char input[size], *p=input;
+	int alpha[26] = {0,}, total=0, count=0;
+	scanf("%s", input);
+	while(*p){
+		if(*p >= 'a')	alpha[*p++ - 'a']++;
+		else alpha[*p++ - 'a']++;
+	}
+	for(int a=0;a<26;a++){
+		if(total<alpha[a]){
+			input[0] = (char)(a+65);
+			total = alpha[a];
+			count=0;
+		}
+		else if(total == alpha[a])	count++;
+	}
+	printf("%c", (count>0)?'?':input[0]);
+}
