cdd875789d738c2d348d7275148c7e93edc0b772
Jun1318 <165673368+Jun1318@users.noreply.github.com>
2024-07-18 11:00:15 +0900

    Update c029|20240718.c

--- c029|20240718.c
+++ c029|20240718.c
@@ -1,20 +1,38 @@
+//https://www.acmicpc.net/problem/14460
 #include<stdio.h>
 #define TABLE_MAX 100000
 
 int main(void){
 	int table[TABLE_MAX][2];
-	int num, diff, count=0;
+	int num, diff, total=0;
 	
 	scanf("%d %d", &num, &diff);
-	
+	if(num<1 || num>TABLE_MAX)
+		return 0;
+	if(diff<0 || diff>=num)
+		return 0;
+		
 	for(int i=0;i<num;i++)
 		scanf("%d", &table[i][0]);
 	for(int i=0;i<num;i++)
 		scanf("%d", &table[i][1]);
 		
-	for(int i=0;i<num;i++)
-		if(table[i][0]-table[i][1]>diff || table[i][0]-table[i][1]<-diff)
-			count++;
+	for(int i=0;i<num;i++){
+		int temp_p1=0;
+		
+		while(table[i][0] !=table[temp_p1++][1]);
+		
+		for(int j=0;j<num;j++){
+			int temp_p2=0;
+			
+			if(table[j][0] == table[i][0])
+				continue;
+			while(table[j][0] !=table[temp_p2++][1]);
+			if((j>i && temp_p1>temp_p2) || (j<i && temp_p1<temp_p2))
+				if(table[i][0]-table[i][1]>diff || table[i][0]-table[i][1]<-diff)
+					total++;
+		}
+	}
 	
-	printf("%d", count);
+	printf("%d", total/2);
 }
